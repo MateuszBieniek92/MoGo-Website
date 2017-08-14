3b65dceeb17bcf9bdc7cfb6ba35d987e6296e8df
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-08-14 16:20:12 +0200

    Main section added with hover in bottom of the section.

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,62 @@
 $(function () {
 
+    // devices width    
+
+    const mobile = window.matchMedia("screen and (max-width: 450px)");
+    const desktop = window.matchMedia("screen and (min-width: 900px)");
+    const tablet = window.matchMedia("screen and (min-width: 451px) and (max-width: 899px)");
+
+    // hover main section
+
+    const slideBarOne = $('.slide-bars').children().first();
+    const slideBarTwo = $('.slide-bars').children().eq(1);
+    const slideBarThree = $('.slide-bars').children().eq(2);
+    const slideBarFour = $('.slide-bars').children().eq(3);
+
+    const barOne = slideBarOne.find('.bar').children().eq(0);
+    const barTwo = slideBarTwo.find('.bar').children().eq(1);
+    const barThree = slideBarThree.find('.bar').children().eq(2);
+    const barFour = slideBarFour.find('.bar').children().eq(3);
+
+    slideBarOne.hover(function () {
+        $(this).fadeIn(1000, function () {
+            barOne.addClass('color');
+        });
+    }, function () {
+        barOne.removeClass('color');
+    });
+
+    slideBarTwo.hover(function () {
+        $(this).fadeIn(1000, function () {
+            barOne.removeClass('color');
+            barTwo.addClass('color');
+        });
+    }, function () {
+        barTwo.removeClass('color');
+    });
+
+    slideBarThree.hover(function () {
+        $(this).fadeIn(1000, function () {
+            barOne.removeClass('color');
+            barThree.addClass('color');
+        });
+    }, function () {
+        barThree.removeClass('color');
+    });
+
+    slideBarFour.hover(function () {
+        $(this).fadeIn(1000, function () {
+            barOne.removeClass('color');
+            barFour.addClass('color');
+        });
+    }, function () {
+        barFour.removeClass('color');
+    });
+
+
+
+
+
+
 
 });
